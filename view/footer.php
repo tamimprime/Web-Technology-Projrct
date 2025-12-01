daa64ccb8f6a9b6ed65a589ccf63ee0c95a453c3
Tamim Prime <90611308+tamimprime@users.noreply.github.com>
2025-12-01 13:00:28 +0600

    Delete view directory

--- view/footer.php
+++
@@ -1,5 +0,0 @@
-<footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
-    <div class="container text-center">
-        <small>Copyright &copy; AIUBians</small>
-    </div>
-</footer>
